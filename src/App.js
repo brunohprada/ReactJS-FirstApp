14e77d71d7e20589c899d250d4e9afdb1c12126f
Bruno Prada <brunohprada@hotmail.com>
2022-01-11 16:01:42 -0300

    Github profile app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import GithubImage from './GitHub-Mark-120px-plus.png';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [search, setSearch] = useState('');
+  const [userData, setUserData] = useState();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(`https://api.github.com/users/${search}`)
+      .them(response => response.json())
+      .them(userResponse => setUserData(userResponse));
+  }
+
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+  }
+
   return (
     <div className="container text-center">
       <h1 className="py-5 text-uppercase">Github profile</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
           <div className="input-group">
             <input
               type="text"
               className="form-control"
               required
+              value={search}
+              onChange={handleChange}
             />
             <span className="input-group-btn">
               <button type='submit' className="btn btn-success">
@@ -22,15 +40,36 @@ function App() {
         </div>
       </form>
       <div className="py-5">
-        <img src={GithubImage} className="responsive rounded-circle" alt="" height="200px" />
+        {!userData && (
+          <img
+            src={GithubImage}
+            className="responsive rounded-circle"
+            alt=""
+            height="200px"
+          />
+        )}
+        {userData && (
+          <div>
+            <img
+              src={userData.avatar_url}
+              className="responsive rounded-circle"
+              alt=""
+              height="200px"
+            />
+            <h1 className="pt-5">
+              <a href={userData.html_url} target="_new">
+                {userData.name}
+              </a>
+            </h1>
+            <h3>{userData.location}</h3>
+            <p>
+              <a href={userData.blog} target="_new" className="text-info">
+                {userData.blog}
+              </a>
+            </p>
+          </div>
+        )}
       </div>
-      <h1 className="pt-5">
-        <a href="https://github.com/brunohprada" target="_new">Bruno Prada</a>
-      </h1>
-      <h3>Osasco</h3>
-      <p>
-        <a href="https://github.com" target="_new" className="text-info">https://github.com</a>
-      </p>
     </div>
   );
 }
